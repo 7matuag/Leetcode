b30cfc4860a3e54c27035544a20eb97c7da1295a
Gautam Tomar <116489929+7matuag@users.noreply.github.com>
2023-02-26 10:31:42 +0530

    Update Find the Divisibility Array of a String.cpp

--- CONTESTS/Weekly Contest 334/Find the Divisibility Array of a String.cpp
+++ CONTESTS/Weekly Contest 334/Find the Divisibility Array of a String.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    vector<int> leftRigthDifference(vector<int>& nums) {
-        vector<int>a,b,c;
-        int presum=0,postsum=0;
-        for(int i=0;i<nums.size();i++){
-            presum+=nums[i];
-            a.push_back(presum);
+    vector<int> divisibilityArray(string word, int m) {
+        vector<int>ans;
+        long long int r=0;
+        for(int i=0;i<word.size();i++){
+            long long int x=word[i]-'0';
+            r=(r*10+x)%m;
+            if(r==0){
+                ans.push_back(1);
+            }
+            else{
+                ans.push_back(0);
+            }
         }
-        for(int i=nums.size()-1;i>=0;i--){
-            postsum+=nums[i];
-            b.push_back(postsum);
-        }
-        reverse(b.begin(),b.end());
-        for(int i=0;i<nums.size();i++){
-            c.push_back(abs(a[i]-b[i]));
-        }
-        return c;
+        return ans;
     }
 };
